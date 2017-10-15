0e1d9345184adda2f68f7b70b65486cbde32f7ef
GrachevaElena <alyona-gra98@yandex.ru>
2017-10-15 18:04:56 +0300

    consistent version

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,39 @@
 #include "mpi.h"
 #include <iostream>
 
-int main(int argc, char**argv) {
-	
-	MPI_Init(&argc, &argv);
+const int N = 1000000;
+
+int Consistent(char* c) {
+	int res = 0;
+	int i = 1;
+	for (; i < N && c[i]; i++) {
+		if (c[i-1]!=' ' && c[i] == ' ') res++;
+	}
+	if (!c[i] && c[i - 1] == ' ') res--;
+	return res+1;
+}
+
+int Parallel(char* c, int* argc, char*** argv) {
+
+	MPI_Init(argc, argv);
 
 	std::cout << "Hello" << std::endl;
 
 	MPI_Finalize();
 
-	//std::cin.get();
+	return 0;
+}
+
+
+
+int main(int argc, char**argv) {
+	
+	char c[N];
+
+	std::cin.getline(c, N);
+	std::cout << Consistent(c) << std::endl;
+
+	std::cin.get();
+
+	return 0;
 }
